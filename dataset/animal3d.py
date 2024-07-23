a8c502a59e89f10ffbdc6165ea468785945647ee
watermelon <anhpg@vng.com.vn>
2024-07-23 13:03:18 +0700

    add datasets preprocessing

--- dataset/animal3d.py
+++ dataset/animal3d.py
@@ -11,12 +11,31 @@ class AnnotationItem():
         keypoint_2d:list, reproj_kp_2d:list, joints_2d:list, 
         keypoint_3d:list, joints_3d:list, 
         pose:list, shape:list, shape_extra:list, trans:list,
+        supercategory:int, category:int, with_tail:int, height:int, width:int
         ):
-        pass
+        self.img_path = img_path
+        self.mask_path = mask_path
+        self.rd_mask_path = rd_mask_path
+        self.bbox = bbox
+        self.keypoint_2d = keypoint_2d
+        self.reproj_kp_2d = reproj_kp_2d
+        self.joints_2d = joints_2d
+        self.keypoint_3d = keypoint_3d
+        self.joints_3d = joints_3d
+        self.pose = pose
+        self.shape = shape
+        self.shape_extra = shape_extra
+        self.trans = trans
+        self.supercategory = supercategory
+        self.category = category
+        self.with_tail = with_tail
+        self.height = height
+        self.width = width
+        
 
 class Animal3DDataset(BaseDataset):
 
-    def __init__(self, cfg, is_inference=False, filter_keys=None):
+    def __init__(self, cfg, is_inference=True, filter_keys=None):
         super(Animal3DDataset, self).__init__(cfg, filter_keys)
         self.cfg = cfg
         self.filter_keys = filter_keys
@@ -35,7 +54,7 @@ class Animal3DDataset(BaseDataset):
         print(self.num_images)
 
         # map[AnnotationItem]
-        self.annotations = self.load_annotations(images)
+        self.load_annotations(images)
         # for i, img  in enumerate(images):
         #     print(img)
 
@@ -45,8 +64,32 @@ class Animal3DDataset(BaseDataset):
 
         anno_file = os.path.join(self.data_dir, self.folder_name + '.json')
         
+        if not os.path.isfile(anno_file):
+            print(f"No exist file config cam.json {anno_file}")
+            return
+        else:
+            with open(anno_file, 'r') as json_file:
+                data = json.load(json_file)
 
-        # for i, img_path in enumerate(anno_paths):
+        anno_data = data["data"]
+        for anno in anno_data:
+            item = AnnotationItem(**anno)
+            self.annotations[item.img_path] = item
+        
+        # loop check
+        counter = 0
+        for i, img_path in enumerate(anno_paths):
+            path_items = img_path.split('/')
+            img_key = os.path.join(*path_items[-4:])
+            if img_key not in self.annotations: 
+                print(f"Something wrong with key {img_key}")
+                continue
+            counter += 1
+        
+        if counter != len(self.annotations):
+            print("Missing images")
+        else:
+            print("All images annotated")
 
 
 
